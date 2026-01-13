f2d09344f002a762545515f419670c33c42ce2d1
moh-bash <bashirkuja@gmail.com>
2026-01-13 20:11:05 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,94 @@
+let namegame = document.getElementById("player-name");
+let nameSpan = document.getElementById("name");
+namegame.addEventListener("keyup", function() {
+    nameSpan.innerText = namegame.value;
+});
+let goplay = document.getElementById("new-game");
+goplay.onclick = function() {
+    if (namegame.value === ""|| namegame.value == null) {
+        namegame.placeholder = "Please enter your name";
+        namegame.classList.add("pom");
+        namegame.onkeyup = function() {
+            namegame.classList.remove("pom");
+        }
+    } else {
+        document.querySelector(".controller").style.display = "none";
+        let levelupSound = document.getElementById("levelup");
+        levelupSound.play();
+    }
+}
+
+
+let time = 1000;
+let containerblock = document.getElementById("containerblock");
+let blocks = Array.from(containerblock.children);
+let orderRange = [...Array(blocks.length).keys()];
+shuffle(orderRange)
+
+blocks.forEach((block, index) => {
+    block.style.order = orderRange[index];
+
+    block.addEventListener("click",function(){
+        flipblock(block);
+    });
+});
+
+// Flip function
+function flipblock(selectedblock) {
+    selectedblock.classList.add("flepped");
+
+    let allfleppedblocks = blocks.filter(fleppedblock => fleppedblock.classList.contains("flepped"));
+
+    if (allfleppedblocks.length === 2) {
+        stopclicking();
+        checkmatch(allfleppedblocks[0], allfleppedblocks[1]);
+    }
+    
+};
+
+// Match function
+function checkmatch(block1, block2) {
+    let triesElement = document.getElementById("tries");
+    if (block1.dataset.technology === block2.dataset.technology) {
+        block1.classList.remove("flepped");
+        block2.classList.remove("flepped");
+
+        block1.classList.add("matched");
+        block2.classList.add("matched");
+
+        let successSound = document.getElementById("success");
+        successSound.play();
+    }else{
+        triesElement.innerHTML = parseInt(triesElement.innerHTML) + 1;
+        setTimeout(() => {
+            block1.classList.remove("flepped");
+            block2.classList.remove("flepped");
+        }, time);
+        let errorSound = document.getElementById("error");
+        errorSound.play();
+    }
+};
+
+// Stop clicking function
+function stopclicking() {
+    containerblock.classList.add("no-clicking");
+    setTimeout(() => {
+        containerblock.classList.remove("no-clicking");
+    }, time);
+};
+
+// Shuffle function
+function shuffle(array) {
+    let current = array.length,
+        temp,
+        random;
+
+    while (current > 0) {
+        random = Math.floor(Math.random()*current);
+        current--;
+        temp = array[current];
+        array[current]= array[random];
+        array[random]= temp;
+    }
+    return array;ه
+}
